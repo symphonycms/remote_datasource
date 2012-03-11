9fe2970cb3a15de52d436dceba23a941743b9d00
Brendan Abbott <brendan@bloodbone.ws>
2012-03-11 12:13:12 +1000

    Override default functions for this extension. This will be able to be streamlined when PHP5.2 support is dropped

--- extension.driver.php
+++ extension.driver.php
@@ -4,4 +4,26 @@
 
 	Class Extension_Remote_Datasource extends Extension {
 
+		private static $provides = array();
+
+		public static function registerProviders() {
+			self::$provides = array(
+				'data-sources' => array(
+					'RemoteDatasource' => RemoteDatasource::getName()
+				)
+			);
+
+			return true;
+		}
+
+		public static function providerOf($type = null) {
+			self::registerProviders();
+
+			if(is_null($type)) return self::$provides;
+
+			if(!isset(self::$provides[$type])) return array();
+
+			return self::$provides[$type];
+		}
+
 	}
