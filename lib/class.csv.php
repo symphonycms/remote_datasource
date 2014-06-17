3d686c9ef9fbc97fb4c29bc6d86725cad7f697c9
Nils Hörrmann <post@nilshoerrmann.de>
2014-06-17 09:48:01 +0200

    Coding style clean-up

--- lib/class.csv.php
+++ lib/class.csv.php
@@ -1,63 +1,65 @@
 <?php
 
-	Class CSV {
+Class CSV {
 
-		/**
-		 * Given a CSV file, generate a resulting XML tree
-		 *
-		 * @param string $data
-		 * @return string
-		 */
-		public static function convertToXML($data) {
-			$headers = array();
+    /**
+     * Given a CSV file, generate a resulting XML tree
+     *
+     * @param  string $data
+     * @return string
+     */
+    public static function convertToXML($data)
+    {
+        $headers = array();
 
-			// DOMDocument
-			$doc = new DOMDocument('1.0', 'utf-8');
-			$doc->formatOutput = true;
+        // DOMDocument
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $doc->formatOutput = true;
 
-			$root = $doc->createElement('data');
-			$doc->appendChild($root);
+        $root = $doc->createElement('data');
+        $doc->appendChild($root);
 
-			foreach(str_getcsv($data, PHP_EOL) as $i => $row) {
-				if(empty($row)) continue;
+        foreach (str_getcsv($data, PHP_EOL) as $i => $row) {
+            if(empty($row)) continue;
 
-				if($i == 0) {
-					foreach(str_getcsv($row) as $i => $head) {
-						if(class_exists('Lang')) {
-							$head = Lang::createHandle($head);
-						}
-						$headers[] = $head;
-					}
-				}
-				else {
-					self::addRow($doc, $root, str_getcsv($row), $headers);
-				}
-			}
+            if ($i == 0) {
+                foreach (str_getcsv($row) as $i => $head) {
+                    if (class_exists('Lang')) {
+                        $head = Lang::createHandle($head);
+                    }
+                    $headers[] = $head;
+                }
+            } else {
+                self::addRow($doc, $root, str_getcsv($row), $headers);
+            }
+        }
 
-			$output = $doc->saveXML($doc->documentElement);
-			return trim($output);
-		}
+        $output = $doc->saveXML($doc->documentElement);
 
-		/**
-		 * @param DOMDocument $doc
-		 * @param DOMElement $root
-		 * @param array $row
-		 * @param array $headers
-		 */
-		public static function addRow(DOMDocument $doc, DOMElement $root, $row, $headers) {
-			foreach($row as $column) {
-				// Create <entry><header>value</header></entry>
-				$entry = $doc->createElement('entry');
+        return trim($output);
+    }
 
-				foreach ($headers as $i => $header) {
-					$col = $doc->createElement($header);
-					$col = $entry->appendChild($col);
+    /**
+     * @param DOMDocument $doc
+     * @param DOMElement  $root
+     * @param array       $row
+     * @param array       $headers
+     */
+    public static function addRow(DOMDocument $doc, DOMElement $root, $row, $headers)
+    {
+        foreach ($row as $column) {
+            // Create <entry><header>value</header></entry>
+            $entry = $doc->createElement('entry');
 
-					$value = $doc->createTextNode($row[$i]);
-					$value = $col->appendChild($value);
-				}
+            foreach ($headers as $i => $header) {
+                $col = $doc->createElement($header);
+                $col = $entry->appendChild($col);
 
-				$root->appendChild($entry);
-			}
-		}
-	}
+                $value = $doc->createTextNode($row[$i]);
+                $value = $col->appendChild($value);
+            }
+
+            $root->appendChild($entry);
+        }
+    }
+}
